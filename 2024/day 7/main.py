21a80c742531e6ed8df9996abd315e347fea212d
rmseq <rm.sequeira@campus.fct.unl.pt>
2024-12-07 11:50:23 +0000

    Minor improvements

--- 2024/day 7/main.py
+++ 2024/day 7/main.py
@@ -2,23 +2,21 @@ from typing import Callable
 
 
 def f(curr: int, rest: list[int], ops: list[Callable[[int, int], int]], res: int) -> bool:
-    if not rest: return curr == res
-    return any(f(op(curr, rest[0]), rest[1:], ops, res) for op in ops)
+    return curr == res if not rest else any(f(op(curr, rest[0]), rest[1:], ops, res) for op in ops)
 
 
-def solve_part1(eq: list[(int, list[int])]) -> int:
-    ops = [lambda x, y: x + y, lambda x, y: x * y]
-    return sum(res for res, vals in eq if f(vals[0], vals[1:], ops, res))
+def solve_part1(eqs: list[(int, list[int])]) -> int:
+    return sum(res for res, vals in eqs if f(vals[0], vals[1:], [lambda x, y: x + y, lambda x, y: x * y], res))
 
 
-def solve_part2(eq: list[(int, list[int])]) -> int:
+def solve_part2(eqs: list[(int, list[int])]) -> int:
     ops = [lambda x, y: x + y, lambda x, y: x * y, lambda x, y: int(f"{x}{y}")]
-    return sum(res for res, vals in eq if f(vals[0], vals[1:], ops, res))
+    return sum(res for res, vals in eqs if f(vals[0], vals[1:], ops, res))
 
 
 if __name__ == "__main__":
     with open("../../input/2024/7.txt") as file:
-        equations = [(int(a), list(map(int, b.split()))) for a, b in (line.strip().split(": ") for line in file)]
+        e = [(int(a), list(map(int, b.split()))) for a, b in (line.strip().split(": ") for line in file)]
 
-    print(solve_part1(equations))
-    print(solve_part2(equations))
+    print(solve_part1(e))
+    print(solve_part2(e))
